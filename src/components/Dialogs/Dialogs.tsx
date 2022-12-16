b970cc18e2166c7fef05585e315857cfc8ba7708
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-16 17:15:23 +0500

    CSS for dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,13 +3,34 @@ import c from './Dialogs.module.css';
 
 
 function Dialogs() {
-  return (
+    return (
 
-<div>
-    Dialogs
-</div>
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <div className={c.dialog + ' ' + c.active}>
+                    Rustam
+                </div>
+                <div className={c.dialog}>
+                    Daler
+                </div>
+                <div className={c.dialog}>
+                    Vitaliy
+                </div>
+                <div className={c.dialog}>
+                    Timur
+                </div>
+                <div className={c.dialog}>
+                    Ismail
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className={c.message}>Hi</div>
+                <div className={c.message}>What's up man</div>
+                <div className={c.message}>Tnx I'm fine</div>
+            </div>
+        </div>
 
-  );
+    );
 }
 
 export default Dialogs;
