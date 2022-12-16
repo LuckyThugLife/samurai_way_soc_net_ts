7cac9d1f88294b0f28b332c99f5f60f49ea294a7
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-16 16:48:46 +0500

    NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import c from './Navbar.module.css';
 
 
 function Navbar() {
   return (
         <nav className={c.nav}>
-            <div className={c.item}>
-                <a href="/profile">Profile</a>
+            <div className={`${c.item} ${c.active}`}>
+                <NavLink to="/profile" activeClassName={c.activeLink}>Profile</NavLink>
             </div>
             <div className={`${c.item} ${c.active}`}>
-                <a href="/dialogs">Dialogs</a>
+                <NavLink to="/dialogs" activeClassName={c.activeLink}>Dialogs</NavLink>
             </div>
             <div className={`${c.item} ${c.active}`}>
-                <a href="/messages">Messages</a>
+                <NavLink to="/messages" activeClassName={c.activeLink}>Messages</NavLink>
             </div>
 
-            <div className={c.item}>
-                <a href="/news">News</a>
+            <div className={`${c.item} ${c.active}`}>
+                <NavLink to="/news" activeClassName={c.activeLink}>News</NavLink>
             </div>
-            <div className={c.item}>
-                <a href="/music">Music</a>
+            <div className={`${c.item} ${c.active}`}>
+                <NavLink to="/music" activeClassName={c.activeLink}>Music</NavLink>
             </div>
-            <div className={c.item}>
-                <a href="/settings">Settings</a>
+            <div className={`${c.item} ${c.active}`}>
+                <NavLink to="/settings" activeClassName={c.activeLink}>Settings</NavLink>
             </div>
         </nav>
 
