6418eb94971ec489f872e298186adecd280d4564
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-16 16:00:44 +0500

    add_page_Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 function Profile() {
   return (
-            <div className={c.content}>
+            <div>
                 <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWfzqIDJarlhnyPIuTy4PGzRrJ9YoQDmUhzyxydK4bt9QPtpn2kLdTiZnuY0jo-4gN-J4&usqp=CAU" alt="content"/>
                 </div>
