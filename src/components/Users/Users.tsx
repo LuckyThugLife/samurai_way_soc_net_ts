dae31dac17e58daa005bad9cee87ab15ea477e75
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-02-07 12:55:51 +0500

    Side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,15 +8,18 @@ type PropsType = UsersPropsType
 
 const Users = (props: PropsType) => {
 
-    if (props.users.length === 0) {
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        })
-
-
+    const getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
+
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
             <span>
