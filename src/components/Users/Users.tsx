8f79cf43a920dcee247ef8294b7b536703712119
Rustam Sharipov <rustam87.sharipov@gmail.com>
2023-02-07 18:58:18 +0500

    life cycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,15 +7,12 @@ import userPhoto from "../../assets/images/user.png"
 type PropsType = UsersPropsType
 
 class Users extends React.Component<PropsType, PropsType>{
-
-    constructor(props:PropsType) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
-
     render() {
     return <div>
 {
